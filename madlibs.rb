4c74ed1f961ca8ee20e9ccbcdb4d7bd1661cbaf5
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-10-03 16:27:06 -0700

    Mad Libs project.

---
+++ madlibs.rb
@@ -0,0 +1,11 @@
+
+puts "Enter a color: "
+color = gets.chomp()
+puts "Enter a plural_noun: "
+plural_noun = gets.chomp()
+puts "Enter a person: "
+person = gets.chomp()
+
+puts("Roses are " + color)
+puts(plural_noun + " are blue.")
+puts("I love " + person)
