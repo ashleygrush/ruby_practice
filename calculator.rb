b0de8e2ea140a6f301699601cb31ca0406e5b5fc
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-10-03 17:31:34 -0700

    completed simple ruby calculator.

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,23 @@
 
-puts "Enter a number:"
+puts "Enter first number: "
 num1 = gets.chomp().to_f
-puts "Enter a second number:"
+puts "enter opperator: "
+op = gets.chomp()
+puts "Enter a second number: "
 num2 = gets.chomp().to_f
 
-puts (num1 + num2)
+def calculate(num1,op,num2)
+  if op == "+"
+    puts (num1 + num2)
+  elsif op == "-"
+    puts (num1 - num2)
+  elsif op == "/"
+    puts (num1 / num2)
+  elsif op == "*"
+    puts (num1 * num2)
+  else
+    puts "please enter +, -, / or *"
+    end
+end
+
+calculate(num1, op, num2)
