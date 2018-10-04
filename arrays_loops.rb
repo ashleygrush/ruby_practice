7888d414bc27fff2d064c2985805cb4d8152dad2
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-10-03 19:12:58 -0700

    modified array's file.

--- arrays_loops.rb
+++ arrays_loops.rb
@@ -9,8 +9,11 @@ friends[0] = "Max"
 friends[1] = "Chris"
 friends[2] = "Rachel"
 
-# puts friends[2]
-# puts friends[0, 2]
+# calls name at particular index
+puts friends[2]
+
+# calls names at indexes of 0 - 2
+puts friends[0, 2]
 
 # for/each loop
 friends.each do |person|
@@ -22,6 +25,12 @@ for person in friends
   puts person
 end
 
+# prints 0 - 5
 for index in 0..5
+  puts index
+end
+
+# prints 6 times
+6.times do |index|
   puts index
 end
